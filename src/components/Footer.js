6d089c6b99da65abf9b684e94d8497bdbebe7993
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-01-23 13:00:44 +0200

    Footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from "framer-motion"
+import { Link } from "react-router-dom"
 import { navigation } from '../animations/navbar'
 import { Col, Row, Container } from 'react-bootstrap'
 
@@ -15,7 +16,22 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col>
-                        <p>
+                        <h5 className="smallTitle">Site map</h5>
+                        <Link to="/" className="otherText">Home</Link> <br />
+                        <Link to="/about" className="otherText">About</Link> <br />
+                        <Link to="/projects" className="otherText">Projects</Link> <br />
+                        <Link to="/contact" className="otherText">Contact</Link>
+                    </Col>
+                    <Col>
+                        <h5 className="smallTitle">Info</h5>
+                        <p className="otherText">
+                            lorem ipsun <br />
+                            Location: Espoo, Finland. <br />
+                            Email: placeholder. <br />
+                        </p>
+                    </Col>
+                    <Col>
+                        <p className="otherText">
                             lorem ipsun <br />
                             Location: Espoo, Finland. <br />
                             Email: placeholder. <br />
