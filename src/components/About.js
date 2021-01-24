4e3632ed838af2e4da36cd94e772f782e905da94
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-01-24 13:46:04 +0200

    Filling out about page

--- src/components/About.js
+++ src/components/About.js
@@ -35,15 +35,24 @@ const About = () => {
                     <h5 className="smallTitle">Web Development</h5>
                     <p className="otherText">
                         I have mostly used Javascript with react in the front end and Node.js and MongoDB in the backend.
+                        In the first year at Metropolia i had a one quarter dedicated for web dev which was mostly basics of HTML and Css. 
+                        I enjoyed it so much that i joined the Full Stack Open from University of Helsinki and compleated 
+                        that course with 9 course credits.
                     </p>
                     <h5 className="smallTitle">Game Development</h5>
                     <p className="otherText">
-                        For all my games i have used unity and c#. For vector calculations i have used python and mathlab.
+                        For all my games i use unity and c#. For vector calculations i have used python and mathlab.
+                        After first year i chose to master in Game Applications at Metropolia. I have made countles small
+                        test projects but only a few large ones. My first larger solo project can be found in projects 
+                        page with more info. Right now i am in the process of making a Augmented reality app for android which 
+                        will be ready in the first quarter.
                     </p>
                     <h5 className="smallTitle">Other</h5>
                     <p className="otherText">
-                        In the first year i had a brief introduction to C language when programming a simple robot.
-                        This year i have started learning c++.
+                        In the first year i had a brief introduction to C language when programming a simple robot to
+                        find get itself out of randomized maze. This year i have started learning c++ Object oriented
+                        programming and algorithms. Rigth now i am making a simple chess engine with one other student
+                        as a school assignment.
                     </p>
                 </Jumbotron>
                 <Jumbotron className="bg-info">
