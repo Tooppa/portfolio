b9496243e3c3dce4e019a672d644435e2907ca51
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-01-31 14:36:57 +0200

    build folder

---
+++ build/precache-manifest.5016778178b683a52d7810e7c07df0e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80108b451ec99db1c7150979ed33b99b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8e49256e8ec7990dc322",
+    "url": "/portfolio/static/css/main.0a497b70.chunk.css"
+  },
+  {
+    "revision": "12ea3d9123302531be97",
+    "url": "/portfolio/static/js/2.6d6dc86b.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/portfolio/static/js/2.6d6dc86b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e49256e8ec7990dc322",
+    "url": "/portfolio/static/js/main.ca612ae2.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
